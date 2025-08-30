6f7897fe499967d103f14075e35fde80dd8b67bb
Rahul Sharma <125949979+rahulsharma1919@users.noreply.github.com>
2025-08-31 00:41:40 +0530

    about section complete

--- src/components/Frameworks.jsx
+++ src/components/Frameworks.jsx
@@ -1,8 +1,41 @@
+import { OrbitingCircles } from "./OrbitingCircles";
 
-const Frameworks = () => {
+export function Frameworks() {
+  const skills = [
+    "react",
+    "cplusplus",
+    "tailwindcss",
+    "nodejs",
+    "python",
+    "mysql",
+    "sql",
+    "vitejs",
+    "html5",
+    "css3",
+    "mongodb",
+    "git",
+    "github",
+    "firebase",
+    "bootstrap",
+    "figma",
+    "visualstudiocode",
+  ];
   return (
-    <div>Frameworks</div>
-  )
+    <div className="relative flex h-[15rem] w-full flex-col items-center justify-center">
+      <OrbitingCircles iconSize={40}>
+        {skills.map((skill, index) => (
+          <Icon key={index} src={`assets/logos/${skill}.svg`} />
+        ))}
+      </OrbitingCircles>
+      <OrbitingCircles iconSize={25} radius={100} reverse speed={2}>
+        {skills.reverse().map((skill, index) => (
+          <Icon key={index} src={`assets/logos/${skill}.svg`} />
+        ))}
+      </OrbitingCircles>
+    </div>
+  );
 }
 
-export default Frameworks
+const Icon = ({ src }) => (
+  <img src={src} className="rounded-sm hover:scale-110 duration-200" />
+);
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import Card from "../components/Card";
 import { Globe } from "../components/Globe";
 import CopyEmailButton from "../components/CopyEmailButton";
-import Frameworks from "../components/Frameworks";
+import { Frameworks } from "../components/Frameworks";
 
 const About = () => {
   const grid2Container = useRef();
@@ -100,6 +100,13 @@ const About = () => {
         </div>
         {/* Grid 5 */}
         <div className="grid-default-color grid-5">
+          <div className="z-10 w-[50%]">
+            <p className="headtext">Tech Stack</p>
+            <p className="subtext">
+              I specialize in a variety of languages, frameworks, and tools that
+              allow me to build robust and scalable applications.
+            </p>
+          </div>
           <div className="absolute inset-y-0 md:inset-y-9 w-full h-full start-[50%] md:scale-125">
             <Frameworks />
           </div>
