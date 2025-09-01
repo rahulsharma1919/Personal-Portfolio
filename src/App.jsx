570c706cfa20436685300028b87cf543c575be1a
Rahul Sharma <125949979+rahulsharma1919@users.noreply.github.com>
2025-09-02 00:49:14 +0530

    experience section completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Projects from "./sections/Projects";
+import Experiences from "./sections/Experiences";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Hero />
       <About />
       <Projects />
+      <Experiences />
     </div>
   );
 };
