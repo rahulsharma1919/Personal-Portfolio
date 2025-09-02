1454b7707e3fa7467f57ec86a74467087021078e
Rahul Sharma <125949979+rahulsharma1919@users.noreply.github.com>
2025-09-02 23:58:43 +0530

    footer section completed

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { mySocials } from "../constants";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <section className="flex flex-wrap items-center justify-between gap-5 pb-3 text-sm text-neutral-400 c-space">
+      <div className="mb-4 bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full" />
+      <div className="flex gap-2">
+        <p>Terms & Conditions</p>
+        <p>|</p>
+        <p>Privacy Policy</p>
+      </div>
+      <div className="flex gap-3">
+        {mySocials.map((social, index) => (
+          <a href={social.href} key={index}>
+            <img src={social.icon} alt={social.name} className="w-5 h-5" />
+          </a>
+        ))}
+      </div>
+      <p>© 2025 Rahul. All rights reserved.</p>
+    </section>
+  );
+};
 
-export default Footer
+export default Footer;
